16018abad2a78dfda0862516cd3371e38f74e366
Jamal Gwarada <g4c-ongo@G4C-ma.local>
2023-01-30 21:30:37 +0000

    second commit for gui

--- GUI/cGUI.java
+++ GUI/cGUI.java
@@ -1,36 +1,84 @@
-package GUI;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
-public class cGUI {
+public class cGUI implements ActionListener {
 
     //constructor
     public cGUI() {
         // JButton object - for buttons on the GUI
-        JButton b = new JButton("Send");
+        JButton siButton = new JButton("Sign In");
+        JButton suButton = new JButton("Sign Up");
+
         // JTextField object - for text input on the GUI
-        JTextField textField = new JTextField(20);
+        JTextField userTf = new JTextField(20);
+        userTf.setBounds(100, 20, 165, 25);
+
 
         // JFrame object - main window
-        JFrame f = new JFrame("Client GUI");
+        JFrame frame = new JFrame("Client GUI");
+        JPanel panel = new JPanel();
         // JFrame object - for the set size of the gui window
-        f.setPreferredSize(new Dimension(500, 500));
-        f.setResizable(false);
-        // JPanel object - size of window
-        JPanel p = new JPanel();
-        p.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        p.setLayout(new FlowLayout(FlowLayout.RIGHT));
-        p.add(b);
-        p.add(b,BorderLayout.SOUTH);
+        frame.setPreferredSize(new Dimension(500, 500));
+        // this is to stop the user from resizing the window
+        frame.setResizable(false);
+        // JFRAME object - top of gui outline colour set to orange
+        frame.setBackground(Color.ORANGE);
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
 
-        f.add(p, BorderLayout.CENTER);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.pack();
-        f.setVisible(true);
+        frame.add(panel);
+
+        panel.setBackground(Color.ORANGE);
+        panel.setLayout(null);
+        // JPanel object - size of window
+        //JPanel p = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        // brings the panel to the front of the frame
+        //panel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        panel.add(siButton);
+        //brings the button "send" to the bottom of the gui
+        //panel.add(button,BorderLayout.SOUTH);
+
+        // Label "username"
+        JLabel unLabel = new JLabel("Username");
+        unLabel.setBounds(10, 20, 80, 25);
+        userTf.setBounds(100, 20, 165, 25);
+        panel.add(unLabel);
+        panel.add(userTf);
+
+        // Label "password"
+        JLabel pwLabel = new JLabel("Password");
+        pwLabel.setBounds(10, 50, 80, 25);
+        panel.add(pwLabel);
+        // Text field for password
+        JPasswordField pwTf = new JPasswordField();
+        pwTf.setBounds(100, 50, 165, 25);
+        panel.add(pwTf);
+        // sign in button
+        siButton.setBounds(10, 80, 80, 25);
+        siButton.addActionListener(new cGUI());
+        panel.add(siButton);
+        // sign up button
+        suButton.setBounds(100, 80, 80, 25);
+        panel.add(suButton);
+
+
+        frame.setVisible(true);
+
     }
     public static void main(String[] args) {
         new cGUI();
     }
-}
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        System.out.println("Signed In Succesfully");
+
+    }
+}
